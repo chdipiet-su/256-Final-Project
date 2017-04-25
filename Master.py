836d002f503e62db346bc1526bcad21682b6c8a8
Sophia Hoeke <shoeke@syr.edu>
2017-04-25 16:54:23 -0400

    notes

--- Master.py
+++ Master.py
@@ -1,1 +1,38 @@
-#test file
+#TO DO:
+#find HTML table of sizes
+#STEPS:
+
+#User is asked if they are male or female
+#User is asked if they're shopping for shoes? (Bool=true) 
+#User is asked what their country of residence is (string value)
+#User is asked where the brand they are shopping for is based (string value)
+#Calculate result (using shoe sizes)
+#Return conversion value
+#Print results
+
+#User is asked if they are male or female
+#User is asked if they're shopping for shoes? (Bool=false) 
+#User is asked what their country of residence is (string value)
+#User is asked where the brand they are shopping for is based (string value)
+#Calculate result (using clothing sizes)
+#Return conversion value
+#Print results
+
+#import HTML charts and Pandas stuff
+#4 input statements
+#2 functions to claculate results (shoes or clothing)
+#2 print statements
+
+#SOPHIA
+#
+#
+
+
+#COURTNEY
+#
+#
+
+
+#CHRISTINA
+#
+#
