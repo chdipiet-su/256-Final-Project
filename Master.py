542af1441279195a6c644a2533faddb3953cc992
Christina DiPietro <christinadipietro@Christinas-MacBook-Pro.local>
2017-04-27 13:39:01 -0400

    yay2

--- Master.py
+++ Master.py
@@ -35,30 +35,38 @@ shoes = pd.read_html('http://www.shoemetro.com/t-shoe-size-chart.aspx')
 #COURTNEY
 
 gender = input("Are you a male or female? ")
+gender = gender.lower()
 shoes = input("Are you shopping for shoes? [y/n] ")
+shoes = shoes.lower()
 
 #Male/Yes
 if gender == 'male' or gender =='m' and shoes == 'y' or shoes == 'yes':
     home = input("What is your country of residence? ")
-    location = input("What country is the brand you're shopping for from? ")
+    home = home.lower()
+    size = input(flaot("What size are you?")) 
 #Male/No
 elif gender == 'male' or gender =='m' and shoes == 'n' or shoes == 'no':
     clothes = input("Are you shopping for clothes? [y/n]")
+    clothes = clothes.lower()
     if clothes == 'y' or clothes == 'yes':
         home = input("What is your country of residence? ")
-        location = input("What country is the brand you're shopping for from? ")
+        home = home.lower()
+        size = input(flaot("What size are you?"))       
     else:
         print("Program has quit.")
 #Female/Yes
 elif gender == 'female' or gender =='f' and shoes == 'y' or shoes == 'yes':
     home = input("What is your country of residence? ")
-    location = input("What country is the brand you're shopping for from? ")
+    home = home.lower()
+    size = input(flaot("What size are you?")) 
 #Female/No
 elif gender == 'female' or gender =='f' and shoes == 'n' or shoes == 'no':
     clothes = input("Are you shopping for clothes? [y/n]")
+    clothes = clothes.lower()
     if clothes == 'y' or clothes == 'yes':
         home = input("What is your country of residence? ")
-        location = input("What country is the brand you're shopping for from? ")
+        home = home.lower()
+        size = input(flaot("What size are you?")) 
     else:
         print("Program has quit.")   
 else:
@@ -68,3 +76,22 @@ else:
 #CHRISTINA
 #
 #
+euro = ['germany', 'italy', 'france', 'switzerland', 'poland', 'netherlands', 'the netherlands', 'ukraine', 'greece', 'austria', 'sweden', 'norway', 'malta', 'czech republic', 'belgium', 'iceland', 'finland', 'croatia', 'cyprus', 'romania', 'hungary', 'denmark', 'bulgaria', 'luxembourg', 'monaco', 'slovenia', 'serbia', 'vatican city', 'albania', 'lithuania', 'belarus', 'montenegro', 'estonia', 'moldova', 'slovakia', 'bosnia', 'herzegovina', 'kosovo', 'latvia', 'san marino', 'macedonia', 'liechtenstein', 'andorra', 'gibraltar', 'faroe islands', 'isle of man', 'jersey', 'svalbard', 'jan mayen', 'aland islands']
+uk = ['great britain', 'ireland', 'scotland', 'wales']
+usa = ['usa', 'u.s.a.', 'america', 'north america']
+
+
+"""
+give new_location which is whatever continent their home country is in (eure, uk, or usa)
+match up new_location with size
+
+new_size = shoes[[home, 
+
+
+
+"""
+
+
+
+
+
