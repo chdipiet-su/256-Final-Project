5b52ec023c6b545bd6db332932b24b6fd97f4899
Sophia Hoeke <shoeke@syr.edu>
2017-05-02 17:32:46 -0400

    progress

--- Master.py
+++ Master.py
@@ -2,104 +2,82 @@
 #find HTML table of sizes
 #STEPS:
 #User is asked if they are male or female
-#User is asked if they're shopping for shoes? (Bool=true) 
 #User is asked what their country of residence is (string value)
 #User is asked where the brand they are shopping for is based (string value)
 #Calculate result (using shoe sizes)
 #Return conversion value
 #Print results
 
-#User is asked if they are male or female
-#User is asked if they're shopping for shoes? (Bool=false) 
-#User is asked what their country of residence is (string value)
-#User is asked where the brand they are shopping for is based (string value)
-#Calculate result (using clothing sizes)
-#Return conversion value
-#Print results
+'''
+Notes
+-write more functions up front to shorten input statements
+-for each task there should be simple solution
+-filter dataframe (look in pandas doc towards the bottom)
+-filter by column = us and size = 7 then grab that index key and return the whole row from the dataframe
+-can use OR in loops as long as there are | pipes | around them
+-ln 42 in Pandas doc for filtering except we want 'US' == '7'
+-tables are in order of [0], [1], [2], [3] instead of mens, womens, girls, boys
+'''
 
-#import HTML charts and Pandas stuff
-#4 input statements
-#2 functions to claculate results (shoes or clothing)
-#2 print statements
-
-#SOPHIA
 import lxml.html as LH
 import pandas as pd
 import warnings
 warnings.filterwarnings('ignore')
 
 shoes = pd.read_html('http://www.shoemetro.com/t-shoe-size-chart.aspx')
-print(shoes)
-<<<<<<< HEAD
+#print(shoes)
 
-'''
-Notes
--write more functions up front to shorten input statements
--for each task there should be simple solution
--filter dataframe (look in pandas doc towards the bottom
--filter by column = us and size = 7 then grab that index key and return the whole row from the dataframe
--can use OR in loops as long as there are | pipes | around them
--ln 42 in Pandas doc for filtering except we want 'US' == '7'
--tables are in order of [0], [1], [2], [3] instead of mens, womens, girls, boys
+w_shoes = shoes[2]
+m_shoes = shoes[3]
 
-'''
-
-
-
-
-
-
-
-=======
->>>>>>> upstream/master
-
-#COURTNEY
+conts = {
+    "euro": ['germany', 'italy', 'france', 'switzerland', 'poland', 'netherlands', 'the netherlands', 'ukraine', 'greece', 'austria', 'sweden', 'norway', 'malta', 'czech republic', 'belgium', 'iceland', 'finland', 'croatia', 'cyprus', 'romania', 'hungary', 'denmark', 'bulgaria', 'luxembourg', 'monaco', 'slovenia', 'serbia', 'vatican city', 'albania', 'lithuania', 'belarus', 'montenegro', 'estonia', 'moldova', 'slovakia', 'bosnia', 'herzegovina', 'kosovo', 'latvia', 'san marino', 'macedonia', 'liechtenstein', 'andorra', 'gibraltar', 'faroe islands', 'isle of man', 'jersey', 'svalbard', 'jan mayen', 'aland islands'],
+    "uk": ['great britain', 'ireland', 'scotland', 'wales'],
+    "usa": ['usa', 'u.s.a.', 'america', 'north america']
+}
 
 gender = input("Are you a male or female? ")
 gender = gender.lower()
-shoes = input("Are you shopping for shoes? [y/n] ")
-shoes = shoes.lower()
 
-#Male/Yes
-if gender == 'male' or gender =='m' and shoes == 'y' or shoes == 'yes':
+#Male
+if gender == 'male' or gender =='m':
     home = input("What is your country of residence? ")
     home = home.lower()
-    size = input(flaot("What size are you?")) 
-#Male/No
-elif gender == 'male' or gender =='m' and shoes == 'n' or shoes == 'no':
-    clothes = input("Are you shopping for clothes? [y/n]")
-    clothes = clothes.lower()
-    if clothes == 'y' or clothes == 'yes':
-        home = input("What is your country of residence? ")
-        home = home.lower()
-        size = input(flaot("What size are you?"))       
-    else:
-        print("Program has quit.")
-#Female/Yes
-elif gender == 'female' or gender =='f' and shoes == 'y' or shoes == 'yes':
+    size = input("What size are you? ")       
+#Female
+elif gender == 'female' or gender =='f':
     home = input("What is your country of residence? ")
     home = home.lower()
-    size = input(flaot("What size are you?")) 
-#Female/No
-elif gender == 'female' or gender =='f' and shoes == 'n' or shoes == 'no':
-    clothes = input("Are you shopping for clothes? [y/n]")
-    clothes = clothes.lower()
-    if clothes == 'y' or clothes == 'yes':
-        home = input("What is your country of residence? ")
-        home = home.lower()
-        size = input(flaot("What size are you?")) 
-    else:
-        print("Program has quit.")   
+    size = input("What size are you? ")  
 else:
     print("Program has quit.")
 
+def get_continent_list(home, dict_of_conts):
+    for key in dict_of_conts:
+        if home in dict_of_conts[key]:
+            return dict_of_conts[key]
 
-#CHRISTINA
-#
-#
-euro = ['germany', 'italy', 'france', 'switzerland', 'poland', 'netherlands', 'the netherlands', 'ukraine', 'greece', 'austria', 'sweden', 'norway', 'malta', 'czech republic', 'belgium', 'iceland', 'finland', 'croatia', 'cyprus', 'romania', 'hungary', 'denmark', 'bulgaria', 'luxembourg', 'monaco', 'slovenia', 'serbia', 'vatican city', 'albania', 'lithuania', 'belarus', 'montenegro', 'estonia', 'moldova', 'slovakia', 'bosnia', 'herzegovina', 'kosovo', 'latvia', 'san marino', 'macedonia', 'liechtenstein', 'andorra', 'gibraltar', 'faroe islands', 'isle of man', 'jersey', 'svalbard', 'jan mayen', 'aland islands']
-uk = ['great britain', 'ireland', 'scotland', 'wales']
-usa = ['usa', 'u.s.a.', 'america', 'north america']
+result = get_continent_list(home, conts)
+#print(result)
+
+def get_size_df(get_continent_list, size):
+     return w_shoes[w_shoes[result] == size]
+
+result_2 = get_size_df(get_continent_list, conts)
+print(result_2)
+
+
+    
+
+
+
+
+
+
+
+
+
+
 
 
 """
@@ -115,13 +93,4 @@ Be able to match that varable with the name of the column on the HTML table
 Iterate through that specific column looking for 'size'
 Find where size exists in column under continent of origin on HTML table and return the entire corresponding row in the form of a dataframe
 Create a string sntence to print out that is populated with new variables
-
-
-
-
 """
-
-
-
-
-
